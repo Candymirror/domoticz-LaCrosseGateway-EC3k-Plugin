01f593b1f30ea0be44471c671769ce867b53065d
Candymirror <59502139+Candymirror@users.noreply.github.com>
2024-01-22 19:55:25 +0100

    Add files via upload
    Added source

---
+++ lacrossegateway.py
@@ -0,0 +1,100 @@
+import re
+
+matchList = [
+    ("^\\S+\\s+24", "PCA301"),
+    ("^\\S+\\s+22", "EC3000"),
+    ("^(\\S+\\s+9 |OK\\sWS\\s)", "LaCrosse"),
+    ("^OK\\sEMT7110\\s", "EMT7110"),
+    ("^OK\\sLS\\s", "Level"),
+    ("^OK\\sVALUES\\s", "KeyValueProtocol"),
+    ("^OK\\sCL\\s", "CapacitiveLevel"),
+    ("^\[LaCrosseITPlusRead", "Info"), # LGW command = v
+    ("", "Nodevice"), #
+]
+
+def decodeEC3k(data):
+    regex = re.compile(r'OK (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+)')
+    match = regex.match(data)
+    if match:
+        data = [int(c) for c in match.group().split()[1:]]
+        sensortype = data[0]
+        sensorid = ''.join(f'{i:02X}' for i in [data[1], data[2]]) # str([f'{i:02x}' for i in [data[1], data[2]]])
+        ontime = ((data[3] * 16777216) + (data[4] * 65536) + (data[5] * 256) + data[6])/3600
+        totaltime = ((data[7] * 16777216) + (data[8] * 65536) + (data[9] * 256) + data[10])/3600
+        energy = ((data[11] * 16777216) + (data[12] * 65536) + (data[13] * 256) + data[14])
+        power = ((data[15] * 256) + data[16])/10
+        maxpower = ((data[17] * 256) + data[18])/10
+        resets = data[19]
+        return sensorid, power, energy
+
+def decodeKVP(data):
+    """
+    The output can be:
+    - OK VALUES LGW 8936539 UpTimeSeconds=280010,UpTimeText=3Tg. 5Std. 46Min. 50Sek. ,WIFI=mirrornet,ReceivedFrames=76447,FramesPerMinute=24,RSSI=-19,FreeHeap=24888,LD.Min=0.52,LD.Avg=0.53,LD.Max=26.46,OLED=none
+    """
+
+def decodePCA301(data):
+    regex = re.compile(r"OK 24 (\d+) 4 (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+)")
+    match = regex.match(data)
+    if match:
+        data = [int(c) for c in match.group().split()[1:]]
+        line = match.group().split(" ")
+        sensorid = (str(line[4]).zfill(3) + str(line[5]).zfill(3) + str(line[6]).zfill(3))
+        power = (int(line[8]) * 256 + int(line[9])) / 10.0
+        state = int(line[7])
+        consumption = (int(line[10]) * 256 + int(line[11])) / 100.0
+        return sensorid, power, consumption
+
+def decodeInfo(data):
+    """
+    The output can be:
+    - [LaCrosseITPlusReader.10.1s (RFM12B f:0 r:17241)]
+    - [LaCrosseITPlusReader.10.1s (RFM12B f:0 t:10~3)]
+    - [LaCrosseITPlusReader.Gateway.1.35 (1=RFM69 f:868300 r:8) {IP=192.168.178.40}]
+    - [LaCrosseITPlusReader.Gateway.1.35 (1=RFM69 f:868300 r:20000) {IP=192.168.1.7}]
+    """
+    re_info = re.compile(
+        r'\[(?P<name>\w+\.\w+).(?P<ver>.*) ' +
+        r'\(1=(?P<rfm1name>\w+) (\w+):(?P<rfm1freq>\d+) ' +
+        r'(?P<rfm1mode>.*)\) {IP=(?P<address>.*)}\]')
+
+    info = {
+        'name': None,
+        'version': None,
+        'address': None,
+        'rfm1name': None,
+        'rfm1frequency': None,
+        'rfm1datarate': None,
+        'rfm1toggleinterval': None,
+        'rfm1togglemask': None,
+    }
+    match = re_info.match(data)
+    if match:
+        info['name'] = match.group('name')
+        info['version'] = match.group('ver')
+        info['address'] = match.group('address')
+        info['rfm1name'] = match.group('rfm1name')
+        info['rfm1frequency'] = match.group('rfm1freq')
+        values = match.group('rfm1mode').split(':')
+        if values[0] == 'r':
+            info['rfm1datarate'] = values[1]
+        elif values[0] == 't':
+            toggle = values[1].split('~')
+            info['rfm1toggleinterval'] = toggle[0]
+            info['rfm1togglemask'] = toggle[1]
+
+    return info
+
+def parse(Data):
+    data = Data.decode('utf-8').strip('\r\n')
+    for pattern, sensortype in matchList:
+        if re.search(pattern, data):
+            return sensortype, data
+    else:
+        return None
+
+#def set_radio(mode):
+#    """Write to socket."""
+#    data = "20000#1r"
+#    self.sock.send((data + '\r\n').encode())
+#    logging.info("Setting LGW radio to listen to ec3k")
---
+++ plugin.py
@@ -0,0 +1,178 @@
+# Hewalex Python Plugin for Domoticz
+#
+# Authors: RvdVeen
+#
+# Based on
+#
+# https://wiki.fhem.de/wiki/LaCrosseGateway_V1.x
+# https://github.com/Innovatone/python-lacrossegateway/blob/master/pylacrossegateway/lacrossegateway.py
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+# USA
+"""
+<plugin key="LGW" name="LaCrosseGateway" author="RvdVeen" version="0.1">
+    <description>
+        <h2>Domoticz LaCrosseGateway Plugin</h2><br/>
+        <h3>Features</h3>
+        <ul style="list-style-type:square">
+            <li>Domoticz plugin implementation for decoding ec3k messages received by a LaCrosseGateway</li>
+            <li>Decoding for other devices is not tested and may require setting the LGW in another mode</li>
+        </ul>
+    </description>
+    <params>
+        <param field="Address" label="IP Address" width="200px" required="true"/>
+        <param field="Port" label="Port" width="30px" required="true" default="8899"/>
+        <param field="Mode1" label="Device & Mode" width="200px" required="true">
+            <options>
+                <option label="EC3k" value="1" default="EC3k"/>
+            </options>
+        </param>
+        <param field="Mode2" label="Query interval" width="75px" required="true">
+            <options>
+                <option label="15 sec" value="3"/>
+                <option label="30 sec" value="6"/>
+                <option label="1 min" value="12" default="true"/>
+                <option label="3 min" value="36"/>
+                <option label="5 min" value="60"/>
+                <option label="10 min" value="120"/>
+                <option label="30 min" value="360"/>
+                <option label="60 min" value="720"/>
+            </options>
+        </param>
+        <param field="Mode6" label="Debug" width="150px">
+            <options>
+                <option label="True" value="Debug"/>
+                <option label="False" value="Normal"  default="true" />
+            </options>
+        </param>
+    </params>
+</plugin>
+"""
+#import DomoticzEx
+#import DomoticzEx as Domoticz
+import re
+import lacrossegateway
+import Domoticz
+
+class BasePlugin:
+    enabled = False
+    def __init__(self):
+        #self.var = 123
+        return
+
+    def onStart(self):
+        if Parameters["Mode6"] == "Debug":
+            Domoticz.Debugging(1)
+
+        self.websocketConn = Domoticz.Connection(Name="websocketConn", Transport="TCP/IP", 
+                                               Address=Parameters["Address"], Port=Parameters["Port"])
+        self.websocketConn.Connect()
+
+    def onStop(self):
+        Domoticz.Debug("onStop called")
+
+    def onConnect(self, Connection, Status, Description):
+        Domoticz.Debug("onConnect called")
+        data = "20000#1r"
+        self.websocketConn.Send((data + '\r\n').encode())
+        Domoticz.Log("Setting LGW radio to listen to ec3k")
+
+    def onMessage(self, Connection, Data):
+        Domoticz.Debug("onMessage called")
+        Domoticz.Debug(Data)
+        device = lacrossegateway.parse(Data)
+        Domoticz.Debug(device)
+        if device[0] == "EC3000":
+            values = lacrossegateway.decodeEC3k(device[1])
+            Domoticz.Debug(values)
+            ec3kDeviceExists = False
+            for unit in Devices:
+                if Devices[unit].DeviceID == values[0]:
+                    Devices[unit].Update(nValue=0, sValue=str(values[1]) + ";" + str(values[2]))
+                    ec3kDeviceExists = True
+                    Domoticz.Debug("EC3k device with sensorID :"+values[0]+" updated")
+            if ec3kDeviceExists == False:
+                Domoticz.Log("Found new EC3k device with sensorID "+values[0]+ ", trying to add Device")
+                Domoticz.Device(Name="EC3k_"+values[0], DeviceID=values[0], Unit=len(Devices)+1, TypeName="kWh").Create()
+        if device[0] == "Info":
+            values = lacrossegateway.decodeInfo(device[1])
+            Domoticz.Debug(values)
+
+
+    def onCommand(self, DeviceID, Unit, Command, Level, Color):
+        Domoticz.Debug("onCommand called for Device " + str(DeviceID) + " Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))
+
+    def onNotification(self, Name, Subject, Text, Status, Priority, Sound, ImageFile):
+        Domoticz.Debug("Notification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str(Priority) + "," + Sound + "," + ImageFile)
+
+    def onDisconnect(self, Connection):
+        Domoticz.Debug("onDisconnect called")
+
+    def onHeartbeat(self):
+        Domoticz.Debug("onHeartbeat called")
+
+global _plugin
+_plugin = BasePlugin()
+
+def onStart():
+    global _plugin
+    _plugin.onStart()
+
+def onStop():
+    global _plugin
+    _plugin.onStop()
+
+def onConnect(Connection, Status, Description):
+    global _plugin
+    _plugin.onConnect(Connection, Status, Description)
+
+def onMessage(Connection, Data):
+    global _plugin
+    _plugin.onMessage(Connection, Data)
+
+def onCommand(DeviceID, Unit, Command, Level, Color):
+    global _plugin
+    _plugin.onCommand(DeviceID, Unit, Command, Level, Color)
+
+def onNotification(Name, Subject, Text, Status, Priority, Sound, ImageFile):
+    global _plugin
+    _plugin.onNotification(Name, Subject, Text, Status, Priority, Sound, ImageFile)
+
+def onDisconnect(Connection):
+    global _plugin
+    _plugin.onDisconnect(Connection)
+
+def onHeartbeat():
+    global _plugin
+    _plugin.onHeartbeat()
+
+# Generic helper functions
+def DumpConfigToLog():
+    for x in Parameters:
+        if Parameters[x] != "":
+            Domoticz.Debug( "'" + x + "':'" + str(Parameters[x]) + "'")
+    Domoticz.Debug("Device count: " + str(len(Devices)))
+    for DeviceName in Devices:
+        Device = Devices[DeviceName]
+        Domoticz.Debug("Device ID:       '" + str(Device.DeviceID) + "'")
+        Domoticz.Debug("--->Unit Count:      '" + str(len(Device.Units)) + "'")
+        for UnitNo in Device.Units:
+            Unit = Device.Units[UnitNo]
+            Domoticz.Debug("--->Unit:           " + str(UnitNo))
+            Domoticz.Debug("--->Unit Name:     '" + Unit.Name + "'")
+            Domoticz.Debug("--->Unit nValue:    " + str(Unit.nValue))
+            Domoticz.Debug("--->Unit sValue:   '" + Unit.sValue + "'")
+            Domoticz.Debug("--->Unit LastLevel: " + str(Unit.LastLevel))
+    return

69c16c3cdde3c4f03df063c8e3636093701dbb8b
Candymirror <59502139+Candymirror@users.noreply.github.com>
2024-10-02 16:21:34 +0200

    Update lacrossegateway.py
    Added function to decode LaCrosse WeatherStation data. You can now add BME*80 and DHT22 sensors to the LGW. Domoticz should pick these up if you automatically accept new sensors. Only tested with DHT22. LaCrosse wireless WeatherStations should also work. You need to set the correct mode
    
     
    Added sample data as might be shown in the LGW log.

--- lacrossegateway.py
+++ lacrossegateway.py
@@ -3,7 +3,8 @@ import re
 matchList = [
     ("^\\S+\\s+24", "PCA301"),
     ("^\\S+\\s+22", "EC3000"),
-    ("^(\\S+\\s+9 |OK\\sWS\\s)", "LaCrosse"),
+    ("^\\S+\\s+9", "LaCrosse"),
+    ("^OK\\sWS\\s", "LaCrosseWS"),
     ("^OK\\sEMT7110\\s", "EMT7110"),
     ("^OK\\sLS\\s", "Level"),
     ("^OK\\sVALUES\\s", "KeyValueProtocol"),
@@ -11,8 +12,11 @@ matchList = [
     ("^\[LaCrosseITPlusRead", "Info"), # LGW command = v
     ("", "Nodevice"), #
 ]
-
 def decodeEC3k(data):
+    """
+    The output can be:
+    - OK 22 208 36 0 74 46 236 0 74 46 236 0 5 175 116 0 44 42 79 4 0
+    """
     regex = re.compile(r'OK (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+)')
     match = regex.match(data)
     if match:
@@ -26,6 +30,52 @@ def decodeEC3k(data):
         maxpower = ((data[17] * 256) + data[18])/10
         resets = data[19]
         return sensorid, power, energy
+
+def decodeLaCrosse(data):
+    """
+    The output can be:
+    - OK 9 248 1 4 150 106
+    """
+    regex = re.compile(r'OK (\d+) (\d+) (\d+) (\d+) (\d+) (\d+)')
+    match = regex.match(data)
+    if match:
+        data = [int(c) for c in match.group().split()[1:]]
+        sensorid = data[1]
+        sensortype = data[2] & 0x7f
+        new_battery = True if data[2] & 0x80 else False
+        temperature = float(data[3] * 256 + data[4] - 1000) / 10
+        humidity = data[5] & 0x7f
+        low_battery = True if data[5] & 0x80 else False
+        return sensorid, temperature, humidity
+
+def decodeLaCrosseWS(data):
+    """
+    The output can be:
+    - OK WS 0 4 4 96 50 255 255 255 255 255 255 255 255 0 255 255 255 255 255 255 255 255 255 255 255
+    """
+    regex = re.compile(r'OK WS (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+)')
+    match = regex.match(data)
+    if match:
+        data = [int(c) for c in match.group().split()[2:]]
+        typenumber = data[1]
+        new_battery = True if data[13] & 0x01 else False
+        temperature = float(data[2] * 256 + data[3] - 1000) / 10
+        humidity = data[4] & 0x7f
+        #determ hum_status
+        #0=Normal 30-45%
+        #1=Comfortable 45-70%
+        #2=Dry < 30%
+        #3=Wet > 70%
+        if humidity <= 30:
+            hum_status = 2
+        if 30 <= humidity <= 45:
+            hum_status = 0
+        if 45 <= humidity <= 70:
+            hum_status = 1
+        if 70 <= humidity:
+            hum_status = 3
+        low_battery = True if data[13] & 0x04 else False
+        return typenumber, temperature, humidity, hum_status
 
 def decodeKVP(data):
     """
